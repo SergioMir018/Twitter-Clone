bd68431a7c58e90c6bc6d3ae1c0b0bb6bd3a3201
Sergio_Mir <voidtracer6@gmail.com>
2023-10-08 23:11:48 -0400

    MOD: types modified on the props of PostCard component

--- src/app/components/post-card/post-card.tsx
+++ src/app/components/post-card/post-card.tsx
@@ -9,13 +9,18 @@ export default function PostCard ({
   userName,
   avatarUrl,
   content
+}: {
+  user: string | null
+  userName: string | null
+  avatarUrl: string | null
+  content: string | null
 }) {
   return (
     <Card className="shadow-none bg-transparent hover:bg-slate-500/10 transition border-b border-white/20 rounded-none cursor-pointer">
       <CardHeader className="justify-between">
         <div className="flex gap-3">
           <Link href={`/${userName}`}>
-            <Avatar radius="full" size="md" src={avatarUrl} />
+            <Avatar radius="full" size="md" src={avatarUrl ?? ''} />
           </Link>
           <div className="flex flex-col gap-1 items-start justify-center">
             <h4 className="text-small font-semibold leading-none text-default-600">{user}</h4>
